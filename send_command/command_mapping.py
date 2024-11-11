04bbed77b566dcd7336ad09f9e88fc02638638fd
mirovix <miroljubmihailovic98@gmail.com>
2024-11-11 16:35:58 +0100

    feat: from object to command sequence

--- send_command/command_mapping.py
+++ send_command/command_mapping.py
@@ -6,6 +6,12 @@ class CommandMapping:
         for variable, value in mapping_config.items():
             setattr(self, variable, value)
 
+    def get_command_sequence(self) -> str:
+        variables = self.__dict__.items()
+        variables = list(filter(lambda x: x[0] not in ["command", "function"], variables))
+        variables = [str(v) for k, v in variables]
+        return ",".join(variables) + ";"
+
     def __str__(self) -> str:
         lines = [f"{k}: {v}" for k, v in self.__dict__.items()]
         return "\n".join(lines)
