fd814d4118919195c5ddc6b25232989e758b1402
mirovix <miroljubmihailovic98@gmail.com>
2024-11-12 10:24:03 +0100

    feat: socket connection

--- send_command/__main__.py
+++ send_command/__main__.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import asyncio
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from command_web_socket import CommandWebSocket
 from command_main import CommandMain
 
 if __name__ == '__main__':
     command_main = CommandMain(sys.argv)
     command = command_main()
     print(command)
+    web_socket = CommandWebSocket(command.get_command_sequence())
+    asyncio.run(web_socket())
+    exit(0)
