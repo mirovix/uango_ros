3948f312bb0bf551ded51310ea291178f7442918
mirovix <miroljubmihailovic98@gmail.com>
2024-11-14 13:08:13 +0100

    feat: managing time out

--- web_socket/web_socket_main.py
+++ web_socket/web_socket_main.py
@@ -1,21 +1,23 @@
+import asyncio
+
 import websockets
 
 
 class WebSocketMain:
-    def __init__(self, command_sequence: str, web_socket_output: str):
+    def __init__(self, command_sequence: str, web_socket_output: str, timeout: float = 1.0):
         self.command_sequence = command_sequence
         self.web_socket_output = 'ws://' + web_socket_output
+        self.timeout = timeout
 
     async def __call__(self) -> any:
         try:
             async with await websockets.connect(self.web_socket_output) as websocket:
                 await websocket.send(self.command_sequence)
                 print(f"sent: {self.command_sequence}")
-
-                response = await websocket.recv()
-                print("command correctly received")
+                response = await asyncio.wait_for(websocket.recv(), timeout=self.timeout)
                 return response
-
+        except asyncio.TimeoutError:
+            print("command correctly received")
         except Exception as e:
             print(f"An error occurred: {e}")
         return None
